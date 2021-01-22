9ef90620a1d9212469cd8fa4a3244d9222d71346
phil-reliant <pmann@reliant-technology.com>
2021-01-22 15:45:14 -0500

    Exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -31,8 +31,9 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application development"
-  const parts = [
+  const course = {
+    name: "Half Stack application development",
+    parts: [
     {
       name: "Fundamentals of React",
       exercises: 10
@@ -46,12 +47,13 @@ const App = () => {
       exercises: 14
     }
   ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
