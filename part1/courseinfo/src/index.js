4fb49c735eec343fd6c0436dc911925896dd4e1b
phil-reliant <pmann@reliant-technology.com>
2021-01-22 15:20:49 -0500

    exercise 1.1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises: {props.exercises}</p>
+  )
+}
+
 const App = () => {
   const course = "Half Stack application development"
   const part1 = "Fundamentals of React"
@@ -12,17 +32,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises: {exercises1} + {exercises2} + {exercises3} = {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
