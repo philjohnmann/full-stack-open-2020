7454dda47067dc700cdba1efe160aa11d08edba0
phil-reliant <pmann@reliant-technology.com>
2021-01-22 15:09:20 -0500

    Restart

---
+++ part1/src/index-4first-props.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="George"/>
+      <Hello name="Daisy" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
