d86c63dfe4f4f3ca41a8be424768d83a6062bbac
phil-reliant <pmann@reliant-technology.com>
2021-01-25 15:02:44 -0500

    section1d - exercise 1.6

---
+++ part1/src/index-8setclickssimple.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+// starting at 1-d 
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  return (
+    <div>
+      {left}
+      <button onClick={()=>setLeft(left+1)}>
+        left
+      </button>
+      <button onClick={()=>setRight(right+1)}>
+        right
+      </button>
+      {right}
+    </div>
+  )
+
+}
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,29 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne = () => setCounter(counter-1)
-  const setToZero = () => setCounter(0)
-  const Display = ({counter}) => <div><p>{counter}</p></div>
-
-  const Button = ({handleClick, text}) => {
-    return (
-      <button onClick={handleClick}>
-        {text}
-      </button>
-    )
-  }
-
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={decreaseByOne} text="minus" />
-      <Button handleClick={setToZero} text="zero" />
-    </div>
-  )
+  useState(0)
 }
 
 ReactDOM.render(
