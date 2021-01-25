1c154a6b94fb60006e9e86e92ce8c04043b696fb
phil-reliant <pmann@reliant-technology.com>
2021-01-25 13:58:40 -0500

    part 1c complete

---
+++ part1/src/index-6nameagepropsreplacement.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+      <p><br/></p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ part1/src/index-7-setCounter1.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log("rendering...", counter)
+
+  return (
+    <div>{counter}</div>
+  )
+}
+
+let counter = 1
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
