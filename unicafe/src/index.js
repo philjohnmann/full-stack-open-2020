585da0ae809cc0b69cf24fac326b599bf2f5ce6f
phil-reliant <pmann@reliant-technology.com>
2021-01-25 15:34:49 -0500

    exercise 1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -6,18 +6,51 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGood = () => {
-    setGood(good+1)
+  const zeroAll = () => {
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
+    setAll(0)
   }
 
-  const handleNeutral = () => {
-    setNeutral(neutral+1)
+    const handleAll = () => {
+      setAll(all+1)
+    }
+
+    const handleGood = () => {
+      setGood(good+1)
+      handleAll()
+    }
+
+    const handleNeutral = () => {
+      setNeutral(neutral+1)
+      handleAll()
+    }
+
+    const handleBad = () => {
+      setBad(bad+1)
+      handleAll()
+    }
+
+    const ShowStats = () => {
+      let avg = (good-bad)/all
+      let pos = (good)/all
+      if (isNaN(avg)) { avg = 0 }
+      if (isNaN(pos)) { pos = 0 }
+      return (
+      <ul>
+      <li>good {good}</li>
+      <li>neutral {neutral}</li>
+      <li>bad {bad}</li>
+      <li>all {all}</li>
+      <li>average {avg}</li>
+      <li>positive {pos}</li>
+      </ul>
+    )
   }
 
-  const handleBad = () => {
-    setBad(bad+1)
-  }
 
   return (
     <div>
@@ -25,12 +58,10 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
+      <br />
+      <button onClick={zeroAll}>reset</button>
       <h2>statistics</h2>
-      <ul>
-      <li>good {good}</li>
-      <li>neutral {neutral}</li>
-      <li>bad {bad}</li>
-      </ul>
+      <ShowStats />
     </div>
   )
 }
